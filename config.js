7a1cd73e4d006de0f6c4b51e7d085483cc55a63a
Joevita Jose <111225450+joevitajose@users.noreply.github.com>
2026-09-11 19:22:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Fill these in with your own Supabase project details.
 // Project Settings -> API -> Project URL and anon public key.
 
-const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://pbjxnsgzefxwovwftfgn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBianhuc2d6ZWZ4d292d2Z0ZmduIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4OTEzMDU5MywiZXhwIjoyMTA0NzA2NTkzfQ.EO_p0FG4kyq2NzBqlzU4ZsLFmziMAWKOqRKVpAszy00";
